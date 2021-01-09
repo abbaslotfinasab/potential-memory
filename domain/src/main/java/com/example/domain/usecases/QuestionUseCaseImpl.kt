f77fc583c7d7d7bca04bd6cc5cabdcc1eea39bb2
abbaslotfinasab <abbas.lotfinasab2020@gmail.com>
2021-01-09 21:53:10 +0330

    fix issues

--- domain/src/main/java/com/example/domain/usecases/QuestionUseCaseImpl.kt
+++ domain/src/main/java/com/example/domain/usecases/QuestionUseCaseImpl.kt
@@ -2,14 +2,10 @@ package com.example.domain.usecases
 
 import com.example.domain.models.QuestionModel
 import com.example.domain.repositories.QuestionRepo
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
-@Module
-@InstallIn(ApplicationComponent::class)
+
 class QuestionUseCaseImpl @Inject constructor(private val questionRepo: QuestionRepo):
     QuestionUseCase<QuestionModel>{
     override fun execute(): Single<QuestionModel> {

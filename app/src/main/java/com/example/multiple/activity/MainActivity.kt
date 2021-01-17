f92a03a5476296b966a03992e02e9d7a1d021830
abbaslotfinasab <abbas.lotfinasab2020@gmail.com>
2021-01-17 19:07:14 +0330

    create splashscreen

--- app/src/main/java/com/example/multiple/activity/MainActivity.kt
+++ app/src/main/java/com/example/multiple/activity/MainActivity.kt
@@ -1,43 +1,17 @@
  package com.example.multiple.activity
 
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import com.example.multiple.R
-import com.example.multiple.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
  @AndroidEntryPoint
  class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel by viewModels<MainViewModel> ()
-
-     @RequiresApi(Build.VERSION_CODES.M)
      override fun onCreate(savedInstanceState: Bundle?) {
          super.onCreate(savedInstanceState)
          setContentView(R.layout.activity_main)
 
-         val button: Button = findViewById(R.id.startButton)
-         button.setOnClickListener {
-
-             mainViewModel.getData()
-
-         }
-
-         mainViewModel.questionModel.observe(this, {
-             it?.let {
-                 Toast.makeText(
-                     this,
-                     "${it.first}in persian is ${it.second}",
-                     Toast.LENGTH_LONG
-                 ).show()
-
-             }
-         })
      }
 }
---
+++ app/src/main/java/com/example/multiple/activity/SplashScreen.kt
@@ -0,0 +1,14 @@
+package com.example.multiple.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.multiple.R
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
